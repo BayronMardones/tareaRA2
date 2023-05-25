1cae68534a9f4d879dc4f462f8c42aff4d91e4e1
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-05-25 02:36:45 -0400

    servidor y clientes iniciados

--- Host.cpp
+++ Host.cpp
@@ -1,7 +1,71 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <cstdlib>
+//192.168.1.13 autodoxeo de pana
+#define PORT 8080
+
 using namespace std;
 
+class Client{
+
+  public:
+    int cli_sockfd, status;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = { 0 };
+    const char* hello = "Hello from client";
+
+    Client()
+    {
+      if ((cli_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+      {
+	  	  perror("socket failed");
+	  	  exit(EXIT_FAILURE);
+	    }
+      else
+      {
+        cout << "CONGRATULATION :D" << endl;
+      }
+
+      serv_addr.sin_family = AF_INET;
+	    serv_addr.sin_port = htons(PORT);
+      serv_addr.sin_addr.s_addr = inet_addr("192.168.1.13");
+
+	    // Convert IPv4 and IPv6 addresses from text to binary
+	    // form
+	    // if (inet_pton(AF_INET, "192.168.1.13 ", &serv_addr.sin_addr) <= 0)
+      // {
+      //   cout << "Invalid address/ Address not supported " << endl;
+	    // }
+
+      if (connect(cli_sockfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0)
+      {
+        perror("listen");
+	  	  exit(EXIT_FAILURE);
+      }
+      else
+      {
+        cout << "connection finished :D" << endl;
+      }
+		  
+		
+	}
+
+  void enviar()
+  {
+    send(cli_sockfd, hello, strlen(hello), 0);
+    cout << "Hello message sent :D" << endl;
+  }
+
+};
+
 int main() {
-  cout << "Hola" << endl;
+
+  Client cliente;
+  cliente.enviar();
+
+  cout << "fin" << endl;
   return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -1,7 +1,98 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <cstdlib>
+
+#define PORT 8080
+
 using namespace std;
 
+class Server
+{
+  public: 
+    int sockfd, new_sockfd, opt = 1;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    char buffer[1024] = { 0 };
+    int valread;
+
+    Server()
+    {
+      if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+      {
+	  	  perror("socket failed");
+	  	  exit(EXIT_FAILURE);
+	    }
+      else
+      {
+        cout << "CONGRATULATION :D" << endl;
+      }
+  
+      if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+      {
+	    	perror("setsockopt");
+	    	exit(EXIT_FAILURE);
+	    }
+      else
+      {
+        cout << "CONFIGURED SOCKET :D" << endl;
+      }
+  
+      address.sin_family = AF_INET;
+	    address.sin_addr.s_addr = INADDR_ANY;
+	    address.sin_port = htons(PORT);
+  
+	    // Forcefully attaching socket to the port 8080
+	    if (bind(sockfd, (struct sockaddr*)&address, sizeof(address)) < 0)
+      {
+	    	perror("bind failed");
+	    	exit(EXIT_FAILURE);
+	    }
+      else
+      {
+        cout << "CONFIGURED BIND :D" << endl;
+      }
+  
+      if (listen(sockfd, 3) < 0)
+      {
+	  	  perror("listen");
+	  	  exit(EXIT_FAILURE);
+	    }
+      else
+      {
+        cout << "LISTEN CORRECT :D" << endl;
+      }
+  
+      if ((new_sockfd = accept(sockfd, (struct sockaddr*)&address, 
+      (socklen_t*)&addrlen)) < 0)
+      {
+	  	  perror("accept");
+	  	  exit(EXIT_FAILURE);
+	    }
+      else
+      {
+        cout << "SERVER FINISHED :D" << endl;
+      }
+
+  }
+
+  void recibir(){
+    valread = read(new_sockfd, buffer, 1024);
+	  printf("%s\n", buffer);
+  }
+
+};
+
 int main() {
-  cout << "Hola" << endl;
+
+  cout << "STARTING" << endl;
+  Server socket;
+  socket.recibir();
+
+  cout << "BYE ;)" << endl;
+
   return 0;
 }
